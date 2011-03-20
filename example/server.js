74ea4866b303f0f15742aa914cc8c71751e2cc98
Eirik Brandtzæg <eirikb@eirikb.no>
2011-03-20 11:19:48 +0100

    Trying to make it work for nodester

--- example/server.js
+++ example/server.js
@@ -1,7 +1,7 @@
 var http = require('http'),
 url = require('url'),
 path = require('path'),
-dir = path.join(__dirname, '/'),
+dir = path.join('/', path.join(path.dirname(__filename , '/'))),
 nodequery = require(path.normalize(dir + '../lib/nodequery')),
 staticFiles = new (require('node-static')).Server('./example', {
 	cache: false
@@ -10,7 +10,7 @@ staticFiles = new (require('node-static')).Server('./example', {
 nodequery.setup({
 	dir: dir + 'pages',
 	jQuery: dir + 'jquery-1.5.min.js',
-	before: function() {
+	before: function($) {
 		$('head').append('<link href="/static/nodequery.css" rel="stylesheet">').
 		append('<link href="http://fonts.googleapis.com/css?family=Cantarell" rel="stylesheet">').
 		append('<link href="/static/shCoreDefault.css" rel="stylesheet">').
